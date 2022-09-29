c454c0e6bc1cc8dbbe26b283709f5578288b64fb
“Nick <nickmroberts.89@gmail.com>
2022-09-29 16:01:25 +0100

    ..

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,16 +1,36 @@
 console.log("Hello World from script.js!");
 
-const birthYear = 1989;
-//const birthYear = 2016;
-const date = new Date();
-const currentDate = date.getFullYear();
+// const birthYear = 1989;
+// const date = new Date();
+// const currentDate = date.getFullYear();
 
-let currentAge = currentDate - birthYear;
-if (currentAge >= 18) {
-  console.log(
-    `You are ${currentAge} years old, you can grab a beer at the bar!`
-  );
-} else {
-  const yearsLeft = 18 - currentAge;
-  console.log(`Come back in ${yearsLeft} years for a beer!`);
+// let currentAge = currentDate - birthYear;
+// if (currentAge >= 18) {
+//   console.log(
+//     `You are ${currentAge} years old, you can grab a beer at the bar!`
+//   );
+// } else {
+//   const yearsLeft = 18 - currentAge;
+//   console.log(`Come back in ${yearsLeft} years for a beer!`);
+// }
+
+// type conversion
+// let input = "1989";
+// input = Number(input);
+// console.log(input);
+// console.log(String("123456"));
+
+// type coercion
+// console.log("4" + "6" - 7);
+
+// == loose equality operator (does coercion)
+const age = 18;
+if (age == "18") {
+  console.log("They are equal");
+}
+
+// === strict equality operator (doesn't do coercion)
+const anotherAge = 18;
+if (anotherAge === "18") {
+  console.log("They are equal");
 }
